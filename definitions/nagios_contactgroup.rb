fff8ef101b2401c1dfee6c56d1c709a3585012f7
Phil Brookes <phil.brookes@redhat.com>
2015-07-10 12:27:24 +0100

    allow for empty groups

--- definitions/nagios_contactgroup.rb
+++ definitions/nagios_contactgroup.rb
@@ -2,8 +2,12 @@ define :nagios_contactgroup, :name => "", :alias => nil, :members => [], :contac
   contactgroup = {}
   contactgroup['alias'] = params[:alias] || params[:name]
   contactgroup['id'] = params[:name]
-  contactgroup['members'] = params[:members].join(",") unless params[:members].length == 0
-  contactgroup['contactgroup_members'] = params[:contactgroup_members].join(",") unless params[:contactgroup_members].length == 0 
+  if params[:members].length > 0
+	  contactgroup['members'] = params[:members].join(",")
+  end
+  if params[:contactgroup_members].length > 0
+  	contactgroup['contactgroup_members'] = params[:contactgroup_members].join(",") 
+  end
 
   node.default['nagios']['dynamic_contactgroups'] << contactgroup
 end
