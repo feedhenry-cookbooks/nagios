9772d6eb9a6806d4598e378124087926136ae300
Phil Brookes <phil.brookes@redhat.com>
2015-07-10 12:33:11 +0100

    undo changes

--- definitions/nagios_contactgroup.rb
+++ definitions/nagios_contactgroup.rb
@@ -2,12 +2,8 @@ define :nagios_contactgroup, :name => "", :alias => nil, :members => [], :contac
   contactgroup = {}
   contactgroup['alias'] = params[:alias] || params[:name]
   contactgroup['id'] = params[:name]
-  if params[:members].length > 0
-	  contactgroup['members'] = params[:members].join(",")
-  end
-  if params[:contactgroup_members].length > 0
-  	contactgroup['contactgroup_members'] = params[:contactgroup_members].join(",") 
-  end
+  contactgroup['members'] = params[:members].join(",")
+  contactgroup['contactgroup_members'] = params[:contactgroup_members].join(",") 
 
   node.default['nagios']['dynamic_contactgroups'] << contactgroup
 end
