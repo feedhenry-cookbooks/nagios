50bde6938c92b10027b606e95c264e0ba9f9e214
Phil Brookes <phil.brookes@redhat.com>
2015-07-14 14:33:25 +0100

    add notes to services

--- definitions/nagios_service.rb
+++ definitions/nagios_service.rb
@@ -1,10 +1,11 @@
-define :nagios_service, :name => "", :hostgroup_name => "", :id => nil, :use_existing_command => "", :service_template => nil do
+define :nagios_service, :name => "", :hostgroup_name => "", :id => nil, :use_existing_command => "", :service_template => nil, :notes => "No notes" do
   service = {}
 
   service['id']                   = params[:id] || params[:name]
   service['hostgroup_name']       = params[:hostgroup_name]
   service['use_existing_command'] = params[:use_existing_command]
   service['service_template']     = params[:service_template]
+  service['notes']                = params[:notes]
 
   node.default['nagios']['dynamic_services'] << service
 
