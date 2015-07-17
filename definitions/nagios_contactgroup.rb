e517758fb21f608de616b15a9561bc07e4ef52ac
Phil Brookes <phil.brookes@redhat.com>
2015-07-17 15:26:02 +0100

    allow nil contactgroups and members

--- definitions/nagios_contactgroup.rb
+++ definitions/nagios_contactgroup.rb
@@ -1,9 +1,9 @@
-define :nagios_contactgroup, :name => "", :alias => nil, :members => [], :contactgroup_members => [] do
+define :nagios_contactgroup, :name => "", :alias => nil, :members => nil, :contactgroup_members => nil do
   contactgroup = {}
   contactgroup['alias'] = params[:alias] || params[:name]
   contactgroup['id'] = params[:name]
-  contactgroup['members'] = params[:members].join(",")
-  contactgroup['contactgroup_members'] = params[:contactgroup_members].join(",") 
+  contactgroup['members'] = params[:members].join(",") unless params[:members].nil?
+  contactgroup['contactgroup_members'] = params[:contactgroup_members].join(",") unless params[:contactgroup_members].nil?
 
   node.default['nagios']['dynamic_contactgroups'] << contactgroup
 end
